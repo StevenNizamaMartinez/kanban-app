8d664d2ba3977aec79e6c72f761ab513053e3f76
StevenNizamaMartinez <0331172014@unjfsc.edu.pe>
2023-04-15 16:59:11 -0500

    origin add

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "https://kanban-api-lovat.vercel.app",
+  origin: "https://kanban-app-psi.vercel.app/",
   credentials: true,
 }));
 app.get("/", (req, res) => {
