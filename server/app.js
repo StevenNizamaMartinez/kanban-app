ee9730e6659e8ea484e7de3b86343ba0df14f782
StevenNizamaMartinez <0331172014@unjfsc.edu.pe>
2023-04-15 16:36:24 -0500

    cors add

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 
-const allowedOrigins = ['http://localhost:5173',"https://kanban-kl5tcho0v-stevennizamamartinez.vercel.app","https://kanban-app-git-master-stevennizamamartinez.vercel.app"];
+const allowedOrigins = ['http://localhost:5173',"https://kanban-kl5tcho0v-stevennizamamartinez.vercel.app","https://kanban-app-git-master-stevennizamamartinez.vercel.app", "https://kanban-app-psi.vercel.app"];
 
 app.use(cors({
   origin: (origin, callback) => {
