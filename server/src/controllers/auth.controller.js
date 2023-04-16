08db3293979d79f4ffb559eb1e7fde134fbead64
StevenNizamaMartinez <0331172014@unjfsc.edu.pe>
2023-04-16 18:44:04 -0500

    config

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -10,22 +10,26 @@ const cookieConfig = {
   secure: true, // Solo se establecerá en conexiones HTTPS
   sameSite: "none", // Configuración de SameSite en None
   domain: "kanban-app-q1os.vercel.app", // Dominio del servidor sin http o http
-}
+};
+
+const tokenConfig = {
+  expiresIn: 60 * 60 * 24 * 30,
+};
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
-  if (!email || !password) return res.status(400).json({ message: "All fields are required" });
+  if (!email || !password)
+    return res.status(400).json({ message: "All fields are required" });
   try {
     const userDb = await userModel.findOne({ email });
     if (!userDb) return res.status(404).json({ message: "User not found" });
-    const compare = await userModel.verifyPassword(password, userDb.password)
-    if (!compare) return res.status(400).json({ message: "Incorrect password" });
-    const token = jsonwebtoken.sign({userDb}, SECRET, {
-      expiresIn: 60 * 60 * 24, 
-    });
+    const compare = await userModel.verifyPassword(password, userDb.password);
+    if (!compare)
+      return res.status(400).json({ message: "Incorrect password" });
+    const token = jsonwebtoken.sign({ userDb }, SECRET, tokenConfig);
     const serialized = cookie.serialize("token", token);
-    res.cookie("token",serialized,cookieConfig)
-    res.send({token,userDb})
+    res.cookie("token", serialized, cookieConfig);
+    res.send({ token, userDb });
   } catch (error) {
     res.status(500).json({ message: error });
   }
@@ -34,28 +38,25 @@ export const login = async (req, res) => {
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
   try {
-    const userDb = await userModel.findOne({email});
+    const userDb = await userModel.findOne({ email });
     if (userDb) return res.status(400).json({ message: "User already exists" });
     const encryptedPassword = await userModel.encryptPassword(password);
-    const newUser = new userModel({ name, email, password:encryptedPassword });
+    const newUser = new userModel({ name, email, password: encryptedPassword });
     const user = await newUser.save();
-    const token = jsonwebtoken.sign({user}, SECRET, {
-      expiresIn: 60 * 60 * 24, // 24 hours
-    });
+    const token = jsonwebtoken.sign({ user }, SECRET, tokenConfig);
     const serialized = cookie.serialize("token", token);
-    res.cookie("token",serialized,cookieConfig)
-    res.send({token,userDb})
+    res.cookie("token", serialized, cookieConfig);
+    res.send({ token, userDb });
   } catch (error) {
     res.status(500).json({ message: error });
   }
 };
 
 export const logout = async (req, res) => {
-try {
-  res.clearCookie("token");
-  res.send("Logged out");
-} catch (error) {
-  res.status(500).json({ message: error });
-}
-
-}
+  try {
+    res.clearCookie("token");
+    res.send("Logged out");
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
