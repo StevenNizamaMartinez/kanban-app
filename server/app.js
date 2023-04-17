a2e6cccbf9a4ae6acaced36ef0eda22be2ea7a2a
StevenNizamaMartinez <0331172014@unjfsc.edu.pe>
2023-04-16 19:20:58 -0500

    add cors ahora es personal

--- server/app.js
+++ server/app.js
@@ -8,6 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
-app.use(corsOptions);
+app.use(cors(corsOptions));
 
 export default app;
